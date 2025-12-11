ef8b32ab7a39b581041deae7961786f93cf357fa
abrahamls <abraham.siahaan1997@gmail.com>
2025-12-12 00:11:51 +0700

    edit base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://172.10.10.27:8080/api';
+const API_BASE = 'https://photo-contest-api-production.up.railway.app/api';
 
 // Create axios instance
 const api = axios.create({
